025c04d6287d2422487bce642164d7f248802eb4
Eirik Kolås <kolaseirik@gmail.com>
2023-01-14 12:13:24 +0100

    started pio project

---
+++ build/nrf52_dk/zephyr/nrf52dk_nrf52832.dts.pre.d
@@ -0,0 +1,12 @@
+empty_file.o: \
+ /home/eirik/.platformio/packages/framework-zephyr/misc/empty_file.c \
+ /home/eirik/.platformio/packages/framework-zephyr/boards/arm/nrf52dk_nrf52832/nrf52dk_nrf52832.dts \
+ /home/eirik/.platformio/packages/framework-zephyr/dts/arm/nordic/nrf52832_qfaa.dtsi \
+ /home/eirik/.platformio/packages/framework-zephyr/dts/common/mem.h \
+ /home/eirik/.platformio/packages/framework-zephyr/dts/arm/nordic/nrf52832.dtsi \
+ /home/eirik/.platformio/packages/framework-zephyr/dts/arm/armv7-m.dtsi \
+ /home/eirik/.platformio/packages/framework-zephyr/dts/common/skeleton.dtsi \
+ /home/eirik/.platformio/packages/framework-zephyr/dts/arm/nordic/nrf_common.dtsi \
+ /home/eirik/.platformio/packages/framework-zephyr/include/dt-bindings/i2c/i2c.h \
+ /home/eirik/.platformio/packages/framework-zephyr/include/dt-bindings/gpio/gpio.h \
+ /home/eirik/.platformio/packages/framework-zephyr/dts/arm/nordic/override.dtsi
